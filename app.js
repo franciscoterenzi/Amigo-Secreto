eda679fe107882a5f316dd923048682ce6fa7e00
¨franciscoterenzi¨ <fterenzidev@gmail.com>
2025-01-25 20:50:23 -0300

    termine la funcion para agregar los nombres, y el ganador del sorteo

--- app.js
+++ app.js
@@ -2,13 +2,44 @@
 
 
 let amigos = []
+let lista =  document.getElementById('listaAmigos');
 
 function agregarAmigo() {
-    let nombre = document.getElementById('amigo').value
-    if (nombre ==''){
+    let nombre = document.getElementById('amigo')
+    if (nombre.value ==''){
         alert('Por favor, inserte un nombre.')
-    }else {
-        amigos.push(nombre)
+        return
     }
- console.log(amigos)   
+    amigos.push(nombre.value)
+    console.log(amigos)   
+    verAmigo(amigos)
+    document.querySelector('#amigo').value = '';
 }
+
+function verAmigo(amigos){
+   
+     lista.innerHTML = ''
+    for(let i = 0; i<amigos.length; i++){
+        let elemento = amigos[i]
+        let li = document.createElement("li");
+        li.innerHTML = elemento;
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo(){
+    if (amigos.length === 0){
+        alert('Ingresa nombres para conocer a tu amigo invisible')
+
+    }else {
+        let ganador = Math.floor(Math.random()*amigos.length);
+        let amigo = amigos[ganador]
+        let li = document.createElement('li');
+        li.textContent = `Tu amigo secreto es ${amigo}`;
+        document.getElementById('resultado').append(li);
+        document.getElementById('listaAmigos').remove('li')
+       
+
+    }
+}
+
