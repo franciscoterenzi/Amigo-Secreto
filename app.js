e1888a3580a8e7703a87722b39bbab9675dc1ca9
¨franciscoterenzi¨ <fterenzidev@gmail.com>
2025-01-25 15:19:07 -0300

    cree el arrego,la validacion para la entrada de nombres

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+let amigos = []
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value
+    if (nombre ==''){
+        alert('Por favor, inserte un nombre.')
+    }else {
+        amigos.push(nombre)
+    }
+ console.log(amigos)   
+}
